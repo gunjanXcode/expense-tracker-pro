06581d046ff7744ccf857698556273a0c4205752
gunjanXcode <gunjanbokde69@gmail.com>
2026-07-09 17:52:04 +0530

    Day 2 Project Setup

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return(
+    <main style={{textAlign:"center", marginTop:"100px"}}>
+      <h1>Expense Tracker Pro</h1>
+      <h2>Welcome to My Expense Tracker Project</h2>
+      <p>Day 2 - Next.js Project Setup Completed Successfully!</p>
+    </main>
+  );
+}
